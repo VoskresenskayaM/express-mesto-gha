66d1aefc09604c5a4fbf52100a6ee7134977d4ef
VoskresenskayaM <vmm459@gmail.com>
2023-06-07 18:05:57 +0500

    fix: test errors4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,19 @@
 const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = require('../utils');
-const NoAccessRightsError = require('../errors/NoAccessRightsError');
+const IncorrectDataUserError = require('../errors/IncorrectDataUserError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new NoAccessRightsError('Необходима авторизация');
-    /* return res
-    .status(401)
-    .send({ message: 'Необходима авторизация' }); */
+    throw new IncorrectDataUserError('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
-    throw new NoAccessRightsError('Необходима авторизация');
-    /* return res
-    .status(401)
-    .send({ message: 'Необходима авторизация' }); */
+    throw new IncorrectDataUserError('Необходима авторизация');
   }
   req.user = payload;
   next();
